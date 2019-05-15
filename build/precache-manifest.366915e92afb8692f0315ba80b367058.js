689b7affb2dca57b7311c0b5070e49f19984edde
住江 祐哉 <yuya@sumienoMacBook-puro.local>
2019-05-15 21:56:01 +0900

    make todo

---
+++ build/precache-manifest.366915e92afb8692f0315ba80b367058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb413c694e3b0e2d7ae9f0e90b4ef4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7146ae098d5c23d2fe8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "a7146ae098d5c23d2fe8",
+    "url": "/static/js/main.13b07403.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+function List(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {props.todo.map( (todo, i) => {
+        return <li key={i}> <input type="button" value="☓"
+                              onClick={() => props.deleteTodo(i)}/> {todo.title}</li>
+      })}
+    </ul>
+  )
+};
+
+class Input extends Component {
+  constructor(props){
+    super(props);
+    this.addTodo = this.addTodo.bind(this);
+  }
+  addTodo(){
+    this.props.addTodo(this.refs.newText.value);
+    this.refs.newText.value='';
+  }
+  render() {
+    return (
+      <div>
+          <input type="text" ref="newText"/>
+          <input type="button" value="追加" onClick={this.addTodo}/>
+      </div>
+    )
+  }
+};
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todo: [
+       { title: 'JavaScript覚える' } ,
+       { title: 'jQuery覚える' } ,
+       { title: 'ES2015覚える' } ,
+       { title: 'React覚える' }
+      ]
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+  // 新規追加
+  addTodo(value) {
+    // 追加
+    this.state.todo.push({
+      title: value
+    });
+    // 保存
+    this.setState({
+      todo : this.state.todo
+    });
+  }
+
+  // 削除機能
+  deleteTodo(i) {
+    // 削除
+    this.state.todo.splice(i, 1);
+    // 保存
+    this.setState({
+      todo : this.state.todo
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>TODOアプリ</h1>
+        <List todo={this.state.todo} deleteTodo={this.deleteTodo}/>
+        <Input addTodo={this.addTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
